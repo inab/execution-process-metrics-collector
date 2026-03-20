inab/execution-process-metrics-collector

commit 33d3ccb902f939211fc5cd925e065696c038d11a

Fixed possible issues uncovered by latest mypy

treecript/parser.py
@@ -171,7 +171,7 @@ def metrics_parser(
     pids["parent"] = pids.apply(
         lambda row: str(row.ppid_create_time) + "_" + str(row.PPID)
         if not pd.isna(row.PPID)
-        else None,
+        else pd.NA,
         axis=1,
     )
 
@@ -211,7 +211,7 @@ def metrics_parser(
     pids["command"] = main_commands
     pids["command_label"] = command_labels
     pids["full_command"] = full_command
-    pids["full_stats"] = full_stats
+    pids["full_stats"] = pd.array(full_stats)
     pids["subtree_root"] = subtree_root
 
     return pids, num_cpu_cores, sampling_period_seconds

commit 2854744621f576bc04c84a8bb1aa8e40f634d90a

Fixed issue #19 , which was introduced in commit 33d3ccb902f939211fc5cd925e065696c038d11a

treecript/parser.py
@@ -211,7 +211,7 @@ def metrics_parser(
     pids["command"] = main_commands
     pids["command_label"] = command_labels
     pids["full_command"] = full_command
-    pids["full_stats"] = pd.array(full_stats)
+    pids["full_stats"] = pd.array(full_stats, dtype=object, copy=False)
     pids["subtree_root"] = subtree_root
 
     return pids, num_cpu_cores, sampling_period_seconds